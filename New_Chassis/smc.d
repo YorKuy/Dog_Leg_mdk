278dc7938402ae5de3cd98b810cf78d89f75c3c7
“YorKuy” <3331639934@qq.com>
2026-05-23 21:39:23 +0800

    5.23腿部控制集成文件leg.cpp

--- New_Chassis/smc.d
+++ New_Chassis/smc.d
@@ -749,4 +749,5 @@ new_chassis/smc.o: ..\Math_Lib\SMC.cpp ..\Math_Lib\SMC.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_spi.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h \
-  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h \
+  ..\RM2023_Lib_V1.2\communication.h ..\Core\Inc\main.h
